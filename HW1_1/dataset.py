6c83d45a015072db3ef8a7800a51d078a99524b5
boewoei0123 <b06902125@ntu.edu.tw>
2021-10-15 15:30:39 +0000

    optimization method backup

--- HW1_1/dataset.py
+++ HW1_1/dataset.py
@@ -1,22 +1,24 @@
+import os
+import glob
 from torch.utils.data import Dataset
 from PIL import Image
 from torchvision.transforms import transforms
 
 
-def CIFAR100(Dataset):
+class CIFAR100(Dataset):
     def __init__(self, args):
         self.images = []
         self.labels = []
         self.names = []
         for f_name in sorted(glob.glob("{}/*".format(args.data_dir))):
-            self.images.append(Image.open(self.f_name))
-            self.labels.append(int(f_name.split('_')[0]))
-            self.names.append(name)
+            self.images.append(Image.open(f_name))
+            self.labels.append(int(os.path.basename(f_name).split('_')[0]))
+            self.names.append(f_name)
         
-        mean = (0.5071, 0.4867, 0.4408)
-        std = (0.2675, 0.2565, 0.2761)
+        self.mean = (0.5071, 0.4867, 0.4408)
+        self.std = (0.2675, 0.2565, 0.2761)
         self.transform = transforms.Compose([transforms.ToTensor(),
-                                            transforms.Normalize(mean, std)])
+                                            transforms.Normalize(self.mean, self.std)])
     
     def __getitem__(self, idx):
         image = self.transform(self.images[idx])
--- HW1_1/generate.py
+++ HW1_1/generate.py
@@ -4,6 +4,9 @@ import logging
 import numpy as np
 import random
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
 from torch.utils.data import DataLoader
 from pytorchcv.model_provider import get_model
 
@@ -32,7 +35,9 @@ if __name__ == "__main__":
     parser.add_argument("--model_names", nargs='+',
                         default=["resnet56_cifar100", "resnet110_cifar100", 
                                 "resnet164bn_cifar100", "densenet100_k24_cifar100"])
-    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--epsilon", type=float, default=8)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--max_iter", type=int, default=100)
     parser.add_argument("--device", type=str, default="cuda:0")
     parser.add_argument("--seed", type=int, default=14)
     args = parser.parse_args()
@@ -50,5 +55,37 @@ if __name__ == "__main__":
     models = []
     for model_name in args.model_names:
         model = get_model(model_name, pretrained=True).to(args.device)
+        model.eval()
         models.append(model)
 
+    lrs = args.epsilon / (255 * torch.Tensor(dataloader.dataset.std)) / args.max_iter
+    all_modifiers = []
+    correct = 0
+    for images, labels, names in dataloader:
+        modifiers = [torch.zeros_like(images[:, ci, :, :], requires_grad=True, device=args.device) 
+                    for ci in range(3)]
+        optimizer = Adam([{"params": m, "lr": lr} for m, lr in zip(modifiers, lrs)])
+        images = images.to(args.device)
+        labels = labels.to(args.device)
+        labels_oh = F.one_hot(labels, num_classes=100).to(args.device)
+        
+        for it in range(args.max_iter):
+            modified_images = images.clone()
+            for ci in range(3):
+                modified_images[:, ci, :, :] += modifiers[ci]
+            preds = model(modified_images)
+            losses = -torch.log(1 - labels_oh * preds.softmax(-1)).sum(-1)
+
+            optimizer.zero_grad()
+            losses.backward(torch.ones_like(losses), retain_graph=True)
+            optimizer.step()
+        #print(modifiers[0].max())
+        #print(modifiers[1].max())
+        #print(modifiers[2].max())
+        
+        modified_images = images.clone()
+        for ci in range(3):
+            modified_images[:, ci, :, :] += modifiers[ci]
+        preds = model(modified_images)
+        correct += (preds.argmax(-1) == labels).sum().item()
+    print(correct / len(dataloader.dataset))
